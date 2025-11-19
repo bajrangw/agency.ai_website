ad788cce9f577c390c390f67e29f622eb9592d9b
Bajrang Wavare <160713621+bajrangw@users.noreply.github.com>
2025-11-19 17:26:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -49,27 +49,30 @@ const App = () => {
   }, []);
 
   return (
-    <div className="dark:bg-black relative">
-      <Toaster />
-      <Navbar theme={theme} setTheme={setTheme} />
-      <Hero />
-      <TrustedBy />
-      <Services />
-      <OurWork />
-      <Teams />
-      <ContactUs />
-      <Footer theme={theme} />
-
+    <>
       <div
         ref={outlineRef}
-        className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]" 
-        style={{transition:'transform 0.1s ease-out'}}></div>
-
+        className="fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]"
+        style={{ transition: "transform 0.1s ease-out" }}
+      ></div>
+  
       <div
         ref={dotRef}
         className="fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]"
       ></div>
-    </div>
+  
+      <div className="dark:bg-black relative overflow-x-hidden">
+        <Toaster />
+        <Navbar theme={theme} setTheme={setTheme} />
+        <Hero />
+        <TrustedBy />
+        <Services />
+        <OurWork />
+        <Teams />
+        <ContactUs />
+        <Footer theme={theme} />
+      </div>
+    </>
   );
 };
 
